a707b8466f0f63130b87ea2797b51104d94c3912
amantaya <andrewantaya@gmail.com>
2019-04-08 11:39:12 -0700

    Intial commit.

---
+++ functions.R
@@ -0,0 +1,4 @@
+# Functions for working with timelapse photo data
+# Design to be read into the R environment and then called when needed by another script
+# Written by Andrew Antaya
+# April 8, 2019
