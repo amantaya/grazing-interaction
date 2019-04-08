c4bb8267ca0c74803ae423cc0d6a6d20a69c72d7
amantaya <andrewantaya@gmail.com>
2019-04-08 11:37:54 -0700

    Deletion.

--- photodatafunctions.R
+++
@@ -1,5 +0,0 @@
-# Functions for working with timelapse photo data
-# Design to be read into the R environment and then called when needed by another script
-# Written by Andrew Antaya
-# April 8, 2019
-
