6113ee29a81082edf3e2a5f7ed288e4aa1c0d0d8
amantaya <andrewantaya@gmail.com>
2020-02-27 12:23:26 -0700

    This commit will change the function as_data_frame() to the function as_tibble() throughout the script. Tibbles are very similar to a dataframe but improve the display behavior inside of the R console. This is very helpful for large datasets where the View() function may cause your system to hang.

--- utilities/extractcamerafolders.R
+++ utilities/extractcamerafolders.R
@@ -1,14 +1,17 @@
 
 library(tidyverse)
 
+# clear the enviroment
+rm(list=ls(all=TRUE))
+
 # check that the current working directory is correct
 getwd()
 
 # set the current working directory to the correct location if it is not correct
-# setwd("K:/")
+setwd("J:/")
 
 # create a list of all sub-folders within the 'cameratraps' folder
-allcamerafolders <- list.dirs(path = "J:/cameratraps", full.names=T)
+allcamerafolders <- list.dirs(path = "J:/cameratraps/aguachiquita", full.names=T)
 
 # view the list of camera trap folders and any sub-folders inside the R environment
 View(allcamerafolders)
@@ -17,24 +20,18 @@ View(allcamerafolders)
 str(allcamerafolders)
 
 # 'allcamerfolders' is a character vector so we should convert it to a dataframe before adding column names
-allcamerafolders.df <- as_data_frame(allcamerafolders)
+allcamerafolders.df <- as_tibble(allcamerafolders)
 
 # rename the first column "value" in the 'allcamerafolders.df' to "path"
 names(allcamerafolders.df)[names(allcamerafolders.df) == "value"] <- "path"
 
-# rm(allcamerafiles)
-
-allcamerafiles <- list.files(path = allcamerafolders.df$path, full.names = TRUE)
-
-write.csv(allcamerafiles, file = "allcamerafiles-2020-02-21.csv", row.names = F)
-
-# View(allcamerafiles)
-
-length(allcamerafiles)
-
 # separate the first column into new columns using the "/" character in the "path" as a separator 
 allcamerafolders.df.separated <- separate(allcamerafolders.df, path, into = c("root", "mainfolder", "locationfolder", "sitefolder", "collectionfolder", "subfolder"), sep = "/", remove = FALSE)
 
+# add an additional column to keep the number of files 
+allcamerafolders.df.separated$numfiles <- NA
+
+# view the new data frame
 View(allcamerafolders.df.separated)
 
 write.csv(allcamerafolders.df.separated, file = "allcamerafolders-2020-02-21.csv", row.names = F)
