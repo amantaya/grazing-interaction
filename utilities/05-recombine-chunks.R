287fef724902b84238efa8401be3e4aaad045185
Andrew Antaya <aantaya@email.arizona.edu>
2021-07-29 13:51:40 -0700

    Intial commit. Add description header.

---
+++ utilities/05-recombine-chunks.R
@@ -0,0 +1,12 @@
+## Horse-Cattle-Elk Grazing Interaction Study Rproj
+## Step 5: Convert Excel Scoring Macro (XLSM) to CSV and Recombine CSV Chunks
+
+## What this script does:
+## Scans a file directory for XLSM files (hopefully completed XLSM)
+## Converts XLSM files to CSV
+## Recombines chunked CSV files (i.e. chunk1.csv, chunk2.csv, chunk3.csv) into a single CSV file
+## Writes out a single CSV file containing all chunks for a collection
+
+## What this script requires:
+## Must specify the location of the folder containing XLSM files
+## Must specify where to write out CSV files
