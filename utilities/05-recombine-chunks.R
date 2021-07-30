15df2eb7555b45e90f1418a710fb0886682ae04e
Andrew Antaya <aantaya@email.arizona.edu>
2021-07-30 13:48:28 -0700

    Convert xlsm files to xlsx, convert xlsx to csv.

--- utilities/05-recombine-chunks.R
+++ utilities/05-recombine-chunks.R
@@ -29,12 +29,42 @@ getwd()
 currentwd <- getwd()
 
 # scan the current working directory for excel macro files
-xlsm <- dir(path = currentwd, pattern = "xlsm")
+xlsm_files <- dir(path = currentwd, pattern = "xlsm")
 
 # read in the xlsm files and convert them to xlsx files
-# this only keeps the first sheet and strips out the macro
-xlsx <- read.xlsx(xlsm, 1)
+# this only keeps the first sheet. It strips out the macro and other sheets
+xlsm_data <- read.xlsx(xlsm_files[1], 1)
 
+# replace the xlsm file extension with xlsx
+# use this vector as the xlsx file names
+xlsx_file_names <- str_replace_all(xlsm_files, "xlsm", "xlsx")
 
-csv <- mapply(convert, xlsm, gsub("xlsm", "csv", xlsm))
+# read in data from xlsm file as a data frame
+# write out data frame as xlsx into sub-directory "xlsx"
+# do this for all xlsm files in a directory (this may take quite awhile)
+for (i in 1:length(xlsm_files)) {
+  xlsm_data <- read.xlsx(xlsm_files[i], 1)
+  write.xlsx(xlsm_data, paste0(currentwd, "/xlsx/", xlsx_file_names[i]), row.names = FALSE)
+}
 
+# play a sound to indicate the loop has completed
+beep("coin")
+
+# scan the current working directory for xlsx files
+xlsx_files <- dir(path = paste0(currentwd, "/xlsx"), pattern = "xlsx")
+
+# replace the xlsx file extension with csv
+# use this vector as our file names for the csv files
+csv_file_names <- str_replace_all(xlsx_file_names, "xlsx", "csv")
+
+# create a sub-directory to store the csv files
+dir.create(paste0(currentwd, "/csv"))
+
+# convert the xlsx files into csv files
+# write out data into the "csv" sub-directory
+for (i in 1:length(xlsx_files)) {
+  rio::convert(paste0(currentwd, "/xlsx/", xlsx_files[i]), 
+               paste0(currentwd, "/csv/", csv_file_names[i])
+               )
+}
+beep("coin")
