74141245ebeedfdaec8bdfbeffd0bb645c263aeb
Andrew Antaya <aantaya@email.arizona.edu>
2021-08-03 17:50:44 -0700

    Convert to DateTime and export csv files by site-year.

--- utilities/06-combine-data-by-site.R
+++ utilities/06-combine-data-by-site.R
@@ -48,7 +48,70 @@ BRL_files <- filter(csv_files_df_separated, sitecode == "BRL")
 BRT_files <- filter(csv_files_df_separated, sitecode == "BRT")
 
 A51_data <- paste(currentwd, A51_files$path, sep = "/") %>% lapply(readr::read_csv) %>% bind_rows()
+BKD_data <- paste(currentwd, BKD_files$path, sep = "/") %>% lapply(readr::read_csv) %>% bind_rows()
+BKN_data <- paste(currentwd, BKN_files$path, sep = "/") %>% lapply(readr::read_csv) %>% bind_rows()
+BKS_data <- paste(currentwd, BKS_files$path, sep = "/") %>% lapply(readr::read_csv) %>% bind_rows()
+BRL_data <- paste(currentwd, BRL_files$path, sep = "/") %>% lapply(readr::read_csv) %>% bind_rows()
+BRT_data <- paste(currentwd, BRT_files$path, sep = "/") %>% lapply(readr::read_csv) %>% bind_rows()
 
+# view the data
 View(A51_data)
 
-A51_2019 <- filter(A51_data, )
+# convert the DateTime column from the Excel DateTime format to the Lubridate format
+A51_data$DateTime <- lubridate::ymd_hms(A51_data$DateTime)
+BKD_data$DateTime <- lubridate::ymd_hms(BKD_data$DateTime)
+BKN_data$DateTime <- lubridate::ymd_hms(BKN_data$DateTime)
+BKS_data$DateTime <- lubridate::ymd_hms(BKS_data$DateTime)
+BRL_data$DateTime <- lubridate::ymd_hms(BRL_data$DateTime)
+BRT_data$DateTime <- lubridate::ymd_hms(BRT_data$DateTime)
+
+
+A51_2019 <- filter(A51_data, year(A51_data$DateTime) == 2019)
+A51_2020 <- filter(A51_data, year(A51_data$DateTime) == 2020)
+
+BKD_2019 <- filter(BKD_data, year(BKD_data$DateTime) == 2019)
+BKD_2020 <- filter(BKD_data, year(BKD_data$DateTime) == 2020)
+
+BKN_2019 <- filter(BKN_data, year(BKN_data$DateTime) == 2019)
+BKN_2020 <- filter(BKN_data, year(BKN_data$DateTime) == 2020)
+
+BKS_2019 <- filter(BKS_data, year(BKS_data$DateTime) == 2019)
+BKS_2020 <- filter(BKS_data, year(BKS_data$DateTime) == 2020)
+
+BRL_2019 <- filter(BRL_data, year(BRL_data$DateTime) == 2019)
+BRL_2020 <- filter(BRL_data, year(BRL_data$DateTime) == 2020)
+
+BRT_2019 <- filter(BRT_data, year(BRT_data$DateTime) == 2019)
+BRT_2020 <- filter(BRT_data, year(BRT_data$DateTime) == 2020)
+
+sitecodes <- unique(csv_files_df_separated$sitecode)
+
+filename_2019 <- paste("A51", "2019.csv", sep = "_")
+filename_2020 <- paste("A51", "2020.csv", sep = "_")
+write_excel_csv(A51_2019, paste(currentwd, filename_2019, sep = "/"))
+write_excel_csv(A51_2020, paste(currentwd, filename_2020, sep = "/"))
+
+filename_2019 <- paste("BKD", "2019.csv", sep = "_")
+filename_2020 <- paste("BKD", "2020.csv", sep = "_")
+write_excel_csv(BKD_2019, paste(currentwd, filename_2019, sep = "/"))
+write_excel_csv(BKD_2020, paste(currentwd, filename_2020, sep = "/"))
+
+filename_2019 <- paste("BKN", "2019.csv", sep = "_")
+filename_2020 <- paste("BKN", "2020.csv", sep = "_")
+write_excel_csv(BKN_2019, paste(currentwd, filename_2019, sep = "/"))
+write_excel_csv(BKN_2020, paste(currentwd, filename_2020, sep = "/"))
+
+filename_2019 <- paste("BKS", "2019.csv", sep = "_")
+filename_2020 <- paste("BKS", "2020.csv", sep = "_")
+write_excel_csv(BKS_2019, paste(currentwd, filename_2019, sep = "/"))
+write_excel_csv(BKS_2020, paste(currentwd, filename_2020, sep = "/"))
+
+filename_2019 <- paste("BRL", "2019.csv", sep = "_")
+filename_2020 <- paste("BRL", "2020.csv", sep = "_")
+write_excel_csv(BRL_2019, paste(currentwd, filename_2019, sep = "/"))
+write_excel_csv(BRL_2020, paste(currentwd, filename_2020, sep = "/"))
+
+filename_2019 <- paste("BRT", "2019.csv", sep = "_")
+filename_2020 <- paste("BRT", "2020.csv", sep = "_")
+write_excel_csv(BRT_2019, paste(currentwd, filename_2019, sep = "/"))
+write_excel_csv(BRT_2020, paste(currentwd, filename_2020, sep = "/"))
