f880bbf79759ac5072cb895468aceb4654866a68
Andrew Antaya <aantaya@email.arizona.edu>
2021-09-09 10:17:20 -0700

    Don't set current folder to working directory
    The "currentfolder" refers to the photo collection folder that is being processed.
    Keep the working directory as the Rproj folder as R scripts are sourced from there.

--- environment.R
+++ environment.R
@@ -5,21 +5,21 @@
 # therefore you likely will need to change the path to working directory
 
 # Copy and paste the path to working directory into the comment below
-# C:\TEMP\quality-check\in-progress\A51_07122020_10112020\chunk3
+# G:\cameratraps\holdingpasture\timelapse\HPL_06212021_07232021
 
 # these objects are required across multiple scripts, do not delete them
-root_folder <- "K:"
+root_folder <- "G:"
 
 main_folder <- "cameratraps"
 
-location_folder <- "fiftyone"
+location_folder <- "holdingpasture"
 
 site_folder <- "timelapse"
 
-collection_folder <- "A51_07122020_10112020"
+collection_folder <- "HPL_06212021_07232021"
 
 subjects_folder <- "subjects"
 
 currentfolder <- file.path(root_folder, main_folder, location_folder, site_folder, collection_folder)
 
-setwd(currentfolder)
+currentwd <- getwd()
