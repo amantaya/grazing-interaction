1f1cf4cfd19d569cbddce5dc732a57d0de0b5414
Andrew Antaya <aantaya@email.arizona.edu>
2022-04-14 14:53:55 -0700

    install pacman on first run then comment out

--- environment.R
+++ environment.R
@@ -1,7 +1,7 @@
 # Use this script to define environment variables that are used across multiple scripts
 
 # Set the working directory to read in the files from the correct location on your computer
-# the files you need to access might be in a different location on your computer 
+# the files you need to access might be in a different location on your computer
 # therefore you likely will need to change the path to working directory
 
 # Copy and paste the path to working directory into the comment below
@@ -14,7 +14,7 @@ full_path_to_collection_folder <- "~/cameratraps/blackcanyon/timelapsesouth/BKS_
 # the reason to configure file paths this way is that Docker uses mounted volumes that do not use drive letters
 # a small bit of logic will switch to drive letter if it is not a Docker container
 # or use the "~" tilde character if it is a Docker container
-# Docker will append the path of the mounted volume before the tilde 
+# Docker will append the path of the mounted volume before the tilde
 is_Docker <- TRUE
 
 # also configure if you are running the scripts on a single collection folder set to TRUE
@@ -48,7 +48,9 @@ subjects_folder <- "subjects"
 # the "~" tilde character is used for mounted volumes on Docker containers
 path_to_collection_folder <- file.path(root_folder, main_folder, location_folder, site_folder, collection_folder)
 
-library(pacman)
+# install.packages("pacman")
+
+library("pacman")
 
 currentwd <- getwd()
 
