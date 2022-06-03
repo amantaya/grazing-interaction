631f4c7d1d4bdd9d136e39da57e7b3ea4f01a9d1
amantaya <aantaya@email.arizona.edu>
2022-06-03 10:29:27 -0700

    bug: need forward slash to file path for container

--- environment.R
+++ environment.R
@@ -14,7 +14,7 @@ usethis::use_git_config(user.name = github_username, user.email = github_email)
 # https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/creating-a-personal-access-token
 credentials::git_credential_ask(repo_url)
 
-# Adds .DS_Store, .Rproj.user, .Rdata, .Rhistory, and .httr-oauth to your global (a.k.a. user-level) .gitignore 
+# Adds .DS_Store, .Rproj.user, .Rdata, .Rhistory, and .httr-oauth to your global (a.k.a. user-level) .gitignore
 usethis::git_vaccinate()
 
 # Check by running a git situation-report:
@@ -28,20 +28,20 @@ usethis::git_sitrep()
 setwd(file.path("", "home", "rstudio", "grazing-interaction"))
 
 # check the current working directory and store it in an object for future reference
-# this is where the code is temporarily stored 
+# this is where the code is temporarily stored
 # IT WILL BE ERASED WHEN THE CONTAINER STOPS
 currentwd <- file.path("", "home", "rstudio", "grazing-interaction")
 
 # Setup file paths to cameratraps data
-path_to_cameratraps_folder <- file.path("home", "rstudio", "work", "data", "iplant", "home", "aantaya", "cameratraps")
+path_to_cameratraps_folder <- file.path("", "home", "rstudio", "work", "data", "iplant", "home", "aantaya", "cameratraps")
 
-path_to_cameratraps2_folder <- file.path("home", "rstudio", "work", "data", "iplant", "home", "aantaya", "cameratraps2")
+path_to_cameratraps2_folder <- file.path("", "home", "rstudio", "work", "data", "iplant", "home", "aantaya", "cameratraps2")
 
 # this is the path to Cyverse user folder
-path_to_user_folder <- file.path("home", "rstudio", "work", "data", "iplant", "home", "aantaya")
+path_to_user_folder <- file.path("", "home", "rstudio", "work", "data", "iplant", "home", "aantaya")
 
 # this is the path to input data that we copied to the Docker container
-path_to_data_input_folder <- file.path("home", "rstudio", "work", "data", "input", "grazing_data", "data")
+path_to_data_input_folder <- file.path("", "home", "rstudio", "work", "data", "input", "grazing_data", "data")
 
 # Setup Environmental Variables -------------------------------------------
 
