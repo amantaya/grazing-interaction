94340522a1325825db684bb34b245d4d0f9b7611
amantaya <aantaya@email.arizona.edu>
2022-06-21 09:20:29 -0700

    add a bind mount for temp data folder

--- scripts/utilities/docker-commands.ps1
+++ scripts/utilities/docker-commands.ps1
@@ -3,9 +3,9 @@
 
 # docker run -d -e DISABLE_AUTH=true --rm -p 127.0.0.1:80:8787 -v C:/Users/andre/Dropbox/Dev/grazing-interaction:/home/rstudio/grazing-interaction amantaya/rocker_verse_grazing:4.0.5
 
-# docker run -d -e DISABLE_AUTH=true --rm -p 127.0.0.1:8787:8787 -v C:/Users/andre/Dropbox/Dev/grazing-interaction:/home/rstudio/grazing-interaction -v H:/cameratraps:/home/rstudio/cameratraps -v H:/cameratraps2:/home/rstudio/cameratraps2 amantaya/rocker-verse:4.0.5
+docker run -d -e DISABLE_AUTH=true --rm -p 127.0.0.1:8787:8787 -v C:/Users/andre/Dropbox/Dev/grazing-interaction:/home/rstudio/grazing-interaction -v G:/cameratraps:/home/rstudio/cameratraps -v G:/cameratraps2:/home/rstudio/cameratraps2 -v G:/temp:/home/rstudio/temp amantaya/rocker-verse:4.0.5
 
-docker run -d -e DISABLE_AUTH=true --rm -p 127.0.0.1:8787:8787 -v C:/Users/andre/Dropbox/Dev/grazing-interaction:/home/rstudio/grazing-interaction amantaya/rocker-verse:4.0.5
+#docker run -d -e DISABLE_AUTH=true --rm -p 127.0.0.1:8787:8787 -v C:/Users/andre/Dropbox/Dev/grazing-interaction:/home/rstudio/grazing-interaction amantaya/rocker-verse:4.0.5
 
 # local r docker container running on port 8787
 # this command disables authentication so only use it on a private network
