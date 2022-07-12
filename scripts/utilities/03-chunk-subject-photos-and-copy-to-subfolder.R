ffcf6b3ed8fb1645ef261ab5606fbb1fa7eb8bb7
amantaya <aantaya@email.arizona.edu>
2022-07-11 18:27:26 -0700

    connect to kanban and for loop through folders

--- scripts/utilities/03-chunk-subject-photos-and-copy-to-subfolder.R
+++ scripts/utilities/03-chunk-subject-photos-and-copy-to-subfolder.R
@@ -1,3 +1,11 @@
+###########################################################################
+###########################################################################
+###                                                                     ###
+###               SECTION 1: BACKGROUND AND CONFIGURATION               ###
+###                                                                     ###
+###########################################################################
+###########################################################################
+
 ## Horse-Cattle-Elk Grazing Interaction Study Rproj
 ## Step 3: Chunk Subject Photos and Copy to Sub-Folder
 
@@ -5,28 +13,115 @@
 ## Reads in the csv file from the photo collection metadata folder
 ## Copies the subject photos from the column into separate folders (chunks)
 ## Writes out a csv for each chunk of photos
-## this csv can then be copied and pasted into the excel macro (HorseImaging2018.xlsm) for scoring 
+## this csv can then be copied and pasted into the excel macro (HorseImaging2018.xlsm) for scoring
 
 ## What this script requires:
-## the csv file from "02-match-subject-photos-and-copy-to-subjects-subfolder.R" 
+## the csv file from "02-match-subject-photos-and-copy-to-subjects-subfolder.R"
 ## this csv file should be located in the "metadata" sub-folder within the collection folder
-## this csv file is named by the photo collection folder and "~matched_subject_photos.csv" 
+## this csv file is named by the photo collection folder and "~matched_subject_photos.csv"
 ## e.g., BRT_11052019_12072019_matched_subject_photos.csv
 
-# start a benchmark to time how long it takes to run this script
-tic("run entire script")
+############################################################################
+############################################################################
+###                                                                      ###
+###                    SECTION 2: SETUP R ENVIRONMENT                    ###
+###                                                                      ###
+############################################################################
+############################################################################
+
+
+# Section 2: Setup R Environment ------------------------------------------
 
 # clear the R environment
-rm(list=ls(all=TRUE))
+# rm(list=ls(all=TRUE))
 
 # set the working directory and environment variables
-source(paste0(getwd(), "/environment.R"))
+source("~/grazing-interaction/environment.R")
 
-# load in the required libraries
-source(paste0(currentwd, "/packages.R"))
+# load in the required packages
+source("~/grazing-interaction/packages.R")
 
-# read in the csv file that contains the metadata for all photos in the collection folder (e.g., BRL_06052019_07022019)
-all_photos_in_collection <- read.csv(paste0(path_to_collection_folder, paste0("/metadata/", collection_folder, "_matched_subject_photos", ".csv")))
+# load in the required functions
+source("~/grazing-interaction/functions.R")
+
+############################################################################
+############################################################################
+###                                                                      ###
+###                 SECTION 3: SELECT FOLDERS TO EXTRACT                 ###
+###                                                                      ###
+############################################################################
+############################################################################
+
+# create a variable to hold the file name in case we switch to a different project
+# and the file name is different we can switch it once here
+file <- "Heber Project Kanban.md"
+
+# read in the kanban board for the Heber project
+heber_project_kanban <- readr::read_lines(
+  file.path(
+    "~",
+    "grazing-interaction",
+    "docs",
+    "heber-project-notes",
+    file
+  ),
+  skip_empty_rows = FALSE,
+  progress = readr::show_progress()
+)
+
+# heading patterns to find
+
+folders_to_chunk_heading_regex <- "##\\sFolders\\sto\\sChunk\\sSubject\\sPhotos"
+
+copy_to_blank_macro_heading_regex <- "##\\sFolders\\sto\\sCopy\\sinto\\sBlank\\sMacro"
+
+# create and index of the two headings
+# we want what's in between the headings
+folders_to_chunk_heading_index <- stringr::str_which(heber_project_kanban, pattern = folders_to_chunk_heading_regex)
+
+copy_to_blank_macro_heading_index <- stringr::str_which(heber_project_kanban, pattern = copy_to_blank_macro_heading_regex)
+
+# add 1 because we don't want to include the first heading
+# subtract 1 because we don't want to include the last heading
+# subset the kanban board using these indexes
+# TODO should I consider not using the index?
+folders_to_chunk <- heber_project_kanban[(folders_to_chunk_heading_index + 2):(copy_to_blank_macro_heading_index - 2)]
+
+folders_to_chunk_regex_pattern <-
+  "([[:upper:]][[:upper:]][[:upper:]]_\\d{8}_\\d{8}|[[:upper:]]\\d{2}_\\d{8}_\\d{8})"
+
+folders_to_chunk_pattern_matches <-
+  stringr::str_extract(folders_to_chunk,
+                       pattern = folders_to_chunk_regex_pattern)
+
+# return only the pattern matches that were not NA
+folders_to_chunk <- folders_to_chunk_pattern_matches[is.na(folders_to_chunk_pattern_matches) == FALSE]
+
+# create a data frame with a "site" column that we can use to construct file paths
+cameratraps_folders_to_chunk <-
+  cameratraps_path_constructor(folders_to_chunk)
+
+# Section 4: Match Subject Photos -----------------------------------------
+############################################################################
+############################################################################
+###                                                                      ###
+###             SECTION 4: Match Subject Photos                          ###
+###                                                                      ###
+############################################################################
+############################################################################
+
+for (i in 1:nrow(cameratraps_folders_to_chunk)) {
+
+# read in the csv file that contains the metadata for all photos in the collection folder
+# (e.g., BRL_06052019_07022019)
+
+
+all_photos_in_collection <- readr::read_csv(
+  file.path(cameratraps_folders_to_chunk$full_path[i],
+                                               "metadata",
+                                               paste0(cameratraps_folders_to_chunk$collection_folder[i],
+                                                   "_matched_subject_photos",
+                                                   ".csv")))
 
 # select only the subject photos from within the collection
 subject_photos_in_collection <- dplyr::filter(all_photos_in_collection, SubjectPhoto == TRUE)
@@ -39,7 +134,8 @@ chunk_size <- 500
 n_rows <- nrow(subject_photos_in_collection)
 
 # the ceiling function takes an single numeric argument x
-# and returns a numeric vector containing the smallest integers not less than the corresponding elements of x.
+# and returns a numeric vector containing the smallest integers
+# not less than the corresponding elements of x.
 # example: ceiling(3.3) = 4
 n_chunks <- ceiling(n_rows/chunk_size)
 
@@ -55,7 +151,7 @@ chunk_names <- paste0("chunk", chunk_number)
 # divide the number of rows in the data frame by the desired chunk size
 # then create a repeating pattern of the chunk number (chunk 1, chunk 2, etc) multiplied by the chunk size
 # i.e. if chunk_size = 500, then chunk 1 will be rep(1, each = 500)
-# then subset the resulting pattern by the number of rows, which will remove the "excess" rows 
+# then subset the resulting pattern by the number of rows, which will remove the "excess" rows
 # created by the last chunk which has less rows than the chunk size
 pattern  <- rep(1:ceiling(n_rows/chunk_size), each = chunk_size)[1:n_rows]
 
@@ -73,35 +169,39 @@ chunks <- split(subject_photos_in_collection, pattern)
 # create a new directory to hold each group of 500 photos
 # these directories will be temporary and can be deleted after scoring each group of photos
 # this for loop creates a new directory for each chunk
-for (i in chunk_number) {
-  dir.create(file.path(path_to_collection_folder, chunk_names[i]))
+for (j in chunk_number) {
+  dir.create(file.path(cameratraps_folders_to_chunk$full_path[i], chunk_names[j]))
 }
 
 # copy the subject photos for each chunk into their corresponding folder
-for (i in chunk_number) {
-  
-  from <- file.path(paste0(path_to_collection_folder, "/", chunks[[i]]$ImageRelative))
-  
-  to <- file.path(path_to_collection_folder, chunk_names[i])
-  
+for (k in chunk_number) {
+
+  from <- file.path(cameratraps_folders_to_chunk$full_path[i], chunks[[k]]$ImageRelative)
+
+  to <- file.path(cameratraps_folders_to_chunk$full_path[i], chunk_names[k])
+
   file.copy(from, to, overwrite = FALSE)
 }
 
-# run the extract-image-paths script on each group of photos to generate 
+# run the extract-image-paths script on each group of photos to generate
 # name each csv file the collection folder and the name of the chunk (e.g., BGT_07302019_09182019_subjects_chunk1.csv)
-for (i in chunk_number) {
-  excelfilename <- paste0(paste(collection_folder, subjects_folder, chunk_names[i], sep = "_"), ".csv")
-  write.csv(chunks[[i]], paste0(path_to_collection_folder, "/metadata/", excelfilename), row.names=FALSE)
+for (l in chunk_number) {
+  excelfilename <- paste0(paste(cameratraps_folders_to_chunk$collection_folder[i],
+                                "subjects",
+                                chunk_names[l], sep = "_"), ".csv")
+  readr::write_csv(chunks[[l]],
+            file.path(cameratraps_folders_to_chunk$full_path[i], "metadata", excelfilename))
 }
 
 # beep("coin")
-toc()
+# toc()
 
 system_time <- Sys.time()
 
 # convert into the correct timezone for your locale (mine is Arizona so we follow Mountain Standard)
 attr(system_time,"tzone") <- "MST"
 
-msg_body <- paste("03-chunk-subject-photos-and-copy-to-subfolder.R", "ran on folder", collection_folder, "completed at", system_time, sep = " ")
+msg_body <- paste("03-chunk-subject-photos-and-copy-to-subfolder.R", "ran on folder", cameratraps_folders_to_chunk$collection_folder[i], "completed at", system_time, sep = " ")
 
 RPushbullet::pbPost(type = "note", title = "Script Completed", body = msg_body)
+}
--- scripts/utilities/04-copy-chunk-to-blank-macro.R
+++ scripts/utilities/04-copy-chunk-to-blank-macro.R
@@ -1,30 +1,123 @@
-# what I want to do
+###########################################################################
+###########################################################################
+###                                                                     ###
+###               SECTION 1: BACKGROUND AND CONFIGURATION               ###
+###                                                                     ###
+###########################################################################
+###########################################################################
 
-# Setup environment by loading in paths to working directories and packages
+# TODO update what this script does
+
+
+############################################################################
+###                                                                      ###
+###                    SECTION 2: SETUP R ENVIRONMENT                    ###
+###                                                                      ###
+############################################################################
+############################################################################
+
+
+# Section 2: Setup R Environment ------------------------------------------
 
 # clear the R environment
-rm(list=ls(all=TRUE))
+# rm(list=ls(all=TRUE))
 
 # set the working directory and environment variables
-source(paste0(getwd(), "/environment.R"))
+source("~/grazing-interaction/environment.R")
 
-# load in the required libraries
-source(paste0(currentwd, "/packages.R"))
+# load in the required packages
+source("~/grazing-interaction/packages.R")
 
-tic("run entire script")
+# load in the required functions
+source("~/grazing-interaction/functions.R")
+
+############################################################################
+############################################################################
+###                                                                      ###
+###                 SECTION 3: SELECT FOLDERS TO EXTRACT                 ###
+###                                                                      ###
+############################################################################
+############################################################################
+
+project_switch <- "heber"
+
+if (project_switch == "heber") {
+  blankmacro <- "HorseImaging-2021-Heber.xlsm"
+} else if (project_switch == "white mountains") {
+  blankmacro <- "HorseImaging-2020-White-Mountains.xlsm"
+} else {
+  warning("The project has not been selected or was entered incorrectly.")
+}
+
+# create a variable to hold the file name in case we switch to a different project
+# and the file name is different we can switch it once here
+file <- "Heber Project Kanban.md"
+
+# read in the kanban board for the Heber project
+heber_project_kanban <- readr::read_lines(
+  file.path(
+    "~",
+    "grazing-interaction",
+    "docs",
+    "heber-project-notes",
+    file
+  ),
+  skip_empty_rows = FALSE,
+  progress = readr::show_progress()
+)
+
+# heading patterns to find
+
+copy_to_blank_macro_heading_regex <- "##\\sFolders\\sto\\sCopy\\sinto\\sBlank\\sMacro"
+
+upload_to_box_scoring_heading_regex <- "##\\sFolders\\sto\\sUpload\\sto\\sBox\\sfor\\sSCORING"
+
+# create and index of the two headings
+# we want what's in between the headings
+copy_to_blank_macro_heading_index <- stringr::str_which(heber_project_kanban, pattern = copy_to_blank_macro_heading_regex)
+
+upload_to_box_scoring_heading_index <- stringr::str_which(heber_project_kanban, pattern = upload_to_box_scoring_heading_regex)
+
+# add 1 because we don't want to include the first heading
+# subtract 1 because we don't want to include the last heading
+# subset the kanban board using these indexes
+# TODO should I consider not using the index?
+folders_to_chunk <- heber_project_kanban[(copy_to_blank_macro_heading_index + 2):(upload_to_box_scoring_heading_index - 2)]
+
+folders_to_chunk_regex_pattern <-
+  "([[:upper:]][[:upper:]][[:upper:]]_\\d{8}_\\d{8}|[[:upper:]]\\d{2}_\\d{8}_\\d{8})"
+
+folders_to_chunk_pattern_matches <-
+  stringr::str_extract(folders_to_chunk,
+                       pattern = folders_to_chunk_regex_pattern)
+
+# return only the pattern matches that were not NA
+folders_to_chunk <- folders_to_chunk_pattern_matches[is.na(folders_to_chunk_pattern_matches) == FALSE]
+
+# create a data frame with a "site" column that we can use to construct file paths
+cameratraps_folders_to_chunk <-
+  cameratraps_path_constructor(folders_to_chunk)
+
+# Section 4: Copy Subjects to Blank Macro ---------------------------------
+
+for (i in 1:nrow(cameratraps_folders_to_chunk)) {
 
 # list all of the chunk subfolders in the collection
-subfolders_in_collection_folder_relative_path <- list.dirs(path_to_collection_folder, full.names = FALSE)
+subfolders_in_collection_folder_relative_path <- list.dirs(cameratraps_folders_to_chunk$full_path[i], full.names = FALSE)
 
-subfolders_in_collection_folder_absolute_path <- list.dirs(path_to_collection_folder, full.names = TRUE)
+subfolders_in_collection_folder_absolute_path <- list.dirs(cameratraps_folders_to_chunk$full_path[i])
 
 # create a wildcard pattern that we can use to match just the "chunk" subfolders
 chunk_wildcard <- "chunk*"
 
 # use a global regular expression to match and return just the "chunk" subfolders
-chunk_subfolders_in_collection_folder <- grep(chunk_wildcard, subfolders_in_collection_folder_relative_path, value = TRUE)
+chunk_subfolders_in_collection_folder <- grep(chunk_wildcard,
+                                              subfolders_in_collection_folder_relative_path,
+                                              value = TRUE)
 
-chunk_subfolders_absolute_paths <- grep(chunk_wildcard, subfolders_in_collection_folder_absolute_path, value = TRUE)
+chunk_subfolders_absolute_paths <- grep(chunk_wildcard,
+                                        subfolders_in_collection_folder_absolute_path,
+                                        value = TRUE)
 
 # number of chunks in the collection folder
 # use this object to indexing
@@ -32,14 +125,17 @@ n_chunks_in_collection_folder <- length(chunk_subfolders_in_collection_folder)
 
 # for loop through each chunk in the collection folder
 
-for (i in (1:n_chunks_in_collection_folder)) {
-  
-  chunk_name <- chunk_subfolders_in_collection_folder[i]
-  
-  path_to_chunk_csv <- file.path(path_to_collection_folder, "metadata", paste0(collection_folder, "_subjects_", chunk_name, ".csv")) 
-  
+for (j in (1:n_chunks_in_collection_folder)) {
+
+  chunk_name <- chunk_subfolders_in_collection_folder[j]
+
+  path_to_chunk_csv <- file.path(cameratraps_folders_to_chunk$full_path[i],
+                                 "metadata",
+                                 paste0(cameratraps_folders_to_chunk$collection_folder[i],
+                                        "_subjects_", chunk_name, ".csv"))
+
   chunk1_csv <- readr::read_csv(path_to_chunk_csv)
-  
+
   # keep only the columns that we want
   # "RecordNumber, ImageFilename, ImagePath, ImageRelative, ImageSize, ImageTime, ImageDate, ImageAlert"
   chunk1_discard_columns <- chunk1_csv %>%
@@ -53,40 +149,41 @@ for (i in (1:n_chunks_in_collection_folder)) {
       ImageDate,
       ImageAlert
     )
-  
+
   # copy the template excel macro from the git repo into the camera trap folder on the external hard drive
-  
+
   from <-
     file.path(currentwd,
+              "templates",
               "excelmacro",
-              "HorseImaging-2020-White-Mountains.xlsm")
-  
-  to <- file.path(path_to_collection_folder, chunk_name)
-  
+              blankmacro)
+
+  to <- file.path(cameratraps_folders_to_chunk$full_path[i], chunk_name)
+
   file.copy(from = from, to = to)
-  
+
   #### Add Warning Message If File Is Not Copied ####
-  
+
   # trim the "/home/rstudio/cameratraps/wildcat/exclosure/WCX_05212019_07102019/101EK113" from the "ImageRelative" column
   # we want to keep just the file name because the excel macro is going to default to relative image paths
   # and altering the rel path to just the filename will tell the macro to look inside just the "chunk" folder
   chunk1_relpaths_split_into_list <-
     stringr::str_split(chunk1_discard_columns$ImageRelative, "/")
-  
+
   keep.last.split.in.file.path <- function(character_vector, pattern) {
     # create an empty object to hold the output of the function
     keep_last_object <- NULL
-    
+
     # string split the character vector on a character or pattern
     all_subjects_string_split <-
       stringr::str_split(character_vector, pattern)
-    
+
     # lengths() detects the number of objects in each list
     # create an index for the last object in a list using the lengths() func
     num_data_objects <- lengths(all_subjects_string_split)
-    
+
     last_object <- num_data_objects
-    
+
     # index the list and keep only the last object
     for (i in 1:length(all_subjects_string_split)) {
       keep_last_object[i] <-
@@ -94,33 +191,33 @@ for (i in (1:n_chunks_in_collection_folder)) {
     }
     return(keep_last_object)
   }
-  
+
   chunk1_discard_columns$ImageRelative <-
     keep.last.split.in.file.path(chunk1_discard_columns$ImageRelative, "/")
-  
+
   # name the macro by the chunk number
   from <-
-    file.path(path_to_collection_folder,
+    file.path(cameratraps_folders_to_chunk$full_path[i],
               chunk_name,
-              "HorseImaging-2020-White-Mountains.xlsm")
-  
+              blankmacro)
+
   xlsm_file_name <-
-    paste0(collection_folder, "_subjects_", chunk_name, ".xlsm")
-  
+    paste0(cameratraps_folders_to_chunk$collection_folder[i], "_subjects_", chunk_name, ".xlsm")
+
   to <-
-    file.path(path_to_collection_folder, chunk_name, xlsm_file_name)
-  
+    file.path(cameratraps_folders_to_chunk$full_path[i], chunk_name, xlsm_file_name)
+
   file.rename(from = from, to = to)
-  
+
   #### Add Warning Message if File Is Not Renamed ####
-  
+
   # copy and paste the data from the csv file into the macro
   # the data from the csv file has been corrected and the unnecessary columns dropped
   xlsm_workbook <-
-    openxlsx::loadWorkbook(file.path(path_to_collection_folder, chunk_name, xlsm_file_name))
-  
+    openxlsx::loadWorkbook(file.path(cameratraps_folders_to_chunk$full_path[i], chunk_name, xlsm_file_name))
+
   chunk1_discard_columns_df <- as.data.frame(chunk1_discard_columns)
-  
+
   # somewhat confusingly, "writeData" writes data to workbook object bound to R
   # but those changes are not yet saved to the file
   openxlsx::writeData(
@@ -131,17 +228,21 @@ for (i in (1:n_chunks_in_collection_folder)) {
     startRow = 2,
     colNames = FALSE
   )
-  
+
   # write out a filled macro
   openxlsx::saveWorkbook(
     xlsm_workbook,
-    file.path(path_to_collection_folder, chunk_name, xlsm_file_name),
+    file.path(cameratraps_folders_to_chunk$full_path[i], chunk_name, xlsm_file_name),
     overwrite = TRUE
   )
 }
 
 # create a new folder to use as a temporary folder for uploading to the cloud
-temp_folder_for_uploading_chunk <- file.path(currentwd, "data", "chunks", collection_folder)
+temp_folder_for_uploading_chunk <- file.path(currentwd,
+                                             "data",
+                                             "temp",
+                                             "chunks",
+                                             cameratraps_folders_to_chunk$collection_folder[i])
 
 dir.create(temp_folder_for_uploading_chunk)
 
@@ -161,6 +262,8 @@ system_time <- Sys.time()
 # convert into the correct timezone for your locale (mine is Arizona so we follow Mountain Standard)
 attr(system_time,"tzone") <- "MST"
 
-msg_body <- paste("04-copy-chunk-to-blank-macro.R", "ran on folder", collection_folder, "completed at", system_time, sep = " ")
+msg_body <- paste("04-copy-chunk-to-blank-macro.R", "ran on folder", cameratraps_folders_to_chunk$collection_folder[i], "completed at", system_time, sep = " ")
 
 RPushbullet::pbPost(type = "note", title = "Script Completed", body = msg_body)
+
+}
