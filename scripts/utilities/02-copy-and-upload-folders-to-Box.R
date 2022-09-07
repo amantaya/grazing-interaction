872a1df98471b600cd7547c6351a9140bfa826b0
amantaya <aantaya@email.arizona.edu>
2022-09-07 13:59:27 -0700

    use `fs` package to copy files

--- scripts/utilities/02-copy-and-upload-folders-to-Box.R
+++ scripts/utilities/02-copy-and-upload-folders-to-Box.R
@@ -5,7 +5,7 @@
 # Setup R Environment -----------------------------------------------------
 
 # clear the R environment
-# rm(list=ls(all=TRUE))
+rm(list=ls(all=TRUE))
 
 # set the working directory and environment variables
 source("~/grazing-interaction/environment.R")
@@ -20,20 +20,21 @@ source("~/grazing-interaction/functions.R")
 
 # create a variable to hold the file name in case we switch to a different project
 # and the file name is different we can switch it once here
-file <- "Heber Project Kanban.md"
+file <- "White Mountains Project Kanban.md"
 
 # read in the kanban board for the Heber project
-heber_project_kanban <- readr::read_lines(
+project_kanban <- readr::read_lines(
   file.path(
     "~",
     "grazing-interaction",
     "docs",
-    "heber-project-notes",
+    "notes",
     file
   ),
   skip_empty_rows = FALSE,
   progress = readr::show_progress()
 )
+
 
 # heading patterns to find
 folders_to_upload_heading_regex <-
@@ -44,17 +45,14 @@ folders_to_add_to_basecamp_heading_regex <-
 
 # create and index of the two headings
 # we want what's in between the headings
-folders_to_upload_heading_index <- stringr::str_which(heber_project_kanban, pattern = folders_to_upload_heading_regex)
+folders_to_upload_heading_index <- stringr::str_which(project_kanban, pattern = folders_to_upload_heading_regex)
 
-folders_to_add_to_basecamp_heading_index <- stringr::str_which(heber_project_kanban, pattern = folders_to_add_to_basecamp_heading_regex)
+folders_to_add_to_basecamp_heading_index <- stringr::str_which(project_kanban, pattern = folders_to_add_to_basecamp_heading_regex)
 
-# add 2 because we don't want to include the first heading and a blank line
-# subtract 2 because we don't want to include the last heading and a blank line
 # subset the kanban board using these indexes
-# TODO should I consider not using the index?
-folders_to_chunk <- heber_project_kanban[(folders_to_upload_heading_index + 2):(folders_to_add_to_basecamp_heading_index - 2)]
+folders_to_chunk <- project_kanban[folders_to_upload_heading_index:folders_to_add_to_basecamp_heading_index]
 
-cameratraps_regex_pattern <- "([[:upper:]][[:upper:]][[:upper:]]_\\d{8}_\\d{8}|A\\d{2}_\\d{8}_\\d{8})"
+cameratraps_regex_pattern <- "([[:upper:]][[:upper:]][[:upper:]]_\\d{8}_\\d{8}|A\\d{2}_\\d{8}_\\d{8}|[[:upper:]][[:upper:]][[:upper:]]_5min_\\d{8}_\\d{8})"
 
 cameratraps2_regex_pattern <- "[[:upper:]][[:upper:]][[:upper:]]\\d{2}_\\d{8}_\\d{8}"
 
@@ -85,16 +83,9 @@ folders_to_copy <- dplyr::bind_rows(cameratraps_folders_to_copy_df, cameratraps2
 
 from <- folders_to_copy$full_path
 
-to <- file.path(path_to_local_data, "temp")
+to <- file.path(path_to_local_data, folders_to_copy$collection_folder)
 
-# file.copy(from = from, to = to, recursive = TRUE)
-
-# TODO 5 minute timelapse photos have a error
-
-for (i in 1:length(from)) {
-  print(paste("Copying...", from[i]))
-  file.copy(from[i], to, recursive = TRUE)
-}
+fs::dir_copy(path = from, new_path = to, overwrite = TRUE)
 
 # get the current system time to notify when the script is completed
 # note that this defaults to UTC (aka Greenwich Mean Time)
