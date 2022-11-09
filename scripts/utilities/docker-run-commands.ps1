3b08b04c203bd6bf8f4b54537269cec68d916b3a
amantaya <aantaya@email.arizona.edu>
2022-11-09 15:34:15 -0700

    add Docker bind mounts for hdd

--- scripts/utilities/docker-run-commands.ps1
+++ scripts/utilities/docker-run-commands.ps1
@@ -2,7 +2,7 @@
 # this command disables authentication so only use it on a localhost/private network
 docker run -d -e DISABLE_AUTH=true -e ROOT=TRUE --rm -p 127.0.0.1:8787:8787 `
 -v C:/Users/andre/Dropbox/Dev/grazing-interaction:/home/rstudio/grazing-interaction `
+-v C:/Users/andre/temp:/home/rstudio/temp `
 -v G:/cameratraps:/home/rstudio/cameratraps `
 -v G:/cameratraps2:/home/rstudio/cameratraps2 `
--v C:/Users/andre/temp:/home/rstudio/temp `
 amantaya/rocker-verse:4.0.5
