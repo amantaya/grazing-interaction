717f4605df7fd526a010358b65ebe7150d92a58f
amantaya <aantaya@email.arizona.edu>
2022-11-15 15:42:54 -0700

    use github PAT inside Docker container

--- environment.R
+++ environment.R
@@ -13,7 +13,7 @@ usethis::use_git_config(user.name = github_username, user.email = github_email)
 # Set your GitHub password
 # If you have 2-FA enabled on your GitHub account, use a Personal Access Token
 # https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/creating-a-personal-access-token
-# credentials::git_credential_ask(repo_url)
+credentials::git_credential_ask(repo_url)
 
 # Adds .DS_Store, .Rproj.user, .Rdata, .Rhistory, and .httr-oauth to your global (a.k.a. user-level) .gitignore
 usethis::git_vaccinate()
