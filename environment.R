2b4d32eff2dc428b8038816df8c98780212e8999
amantaya <aantaya@arizona.edu>
2023-02-22 12:59:24 -0700

    update paths for use outside Docker container

--- environment.R
+++ environment.R
@@ -4,7 +4,7 @@
 # Change this stuff
 repo_url <- "https://github.com/amantaya/grazing-interaction"
 github_username <- "amantaya"
-github_email <- "aantaya@email.arizona.edu"
+github_email <- "aantaya@arizona.edu"
 cyverse_username <- "aantaya"
 
 # Set your git username and email address
@@ -13,7 +13,7 @@ usethis::use_git_config(user.name = github_username, user.email = github_email)
 # Set your GitHub password
 # If you have 2-FA enabled on your GitHub account, use a Personal Access Token
 # https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/creating-a-personal-access-token
-# credentials::git_credential_ask(repo_url)
+credentials::git_credential_ask(repo_url)
 
 # Adds .DS_Store, .Rproj.user, .Rdata, .Rhistory, and .httr-oauth to your global (a.k.a. user-level) .gitignore
 usethis::git_vaccinate()
@@ -24,17 +24,11 @@ usethis::git_sitrep()
 
 # tell Git that the project directory is yours and is safe
 # you may need to start a new R Studio session by clicking the "Power" button at the top-right
-system("git config --global --add safe.directory /home/rstudio/grazing-interaction")
+# system("git config --global --add safe.directory /home/rstudio/grazing-interaction")
 
 # Setup File Paths --------------------------------------------------------
 
-# check the current working directory and store it in an object for future reference
-# the empty quotes causes file.path() to add a backslash before the first folder
-
-currentwd <- file.path("", "home", "rstudio", "grazing-interaction")
-
-# set the working directory to the default mount location in the Docker container
-setwd(currentwd)
+currentwd <- getwd()
 
 # Paths to Data on Cyverse ------------------------------------------------
 
@@ -53,9 +47,9 @@ path_to_data_input_folder <- file.path("", "home", "rstudio", "work", "data", "i
 # Paths to Local Data -----------------------------------------------------
 
 # this is the path to the data folder on a local file system
-path_to_local_data <- file.path("", "home", "rstudio", "temp", "upload")
+path_to_temp_data <- file.path("C:","Users", "andre", "temp")
 
-path_to_local_cameratraps_folder <- file.path("", "home", "rstudio", "cameratraps")
+path_to_external_hdd <- file.path("G:")
 
 # Setup Environmental Variables -------------------------------------------
 
@@ -73,14 +67,14 @@ sessioninfo <- utils::sessionInfo()
 # RPushBullet Setup -------------------------------------------------------
 
 # Enter an API key for the Push Bullet push notification service
-options(rpushbullet.dotfile = "~/grazing-interaction/.rpushbullet.json")
+options(rpushbullet.dotfile = file.path(currentwd, ".rpushbullet.json"))
 
 # warn if you are missing a rpushbullet config file
-if (RPushbullet::pbValidateConf() == FALSE) {
+if (RPushbullet::pbValidateConf(getOption("rpushbullet.dotfile")) == FALSE) {
   RPushbullet::pbSetup()
   }
 
  options(error = function() {
    RPushbullet::pbPost("note", "Error", geterrmessage())
-   if(!interactive()) stop(geterrmessage())
+   if (!interactive()) stop(geterrmessage())
  })
