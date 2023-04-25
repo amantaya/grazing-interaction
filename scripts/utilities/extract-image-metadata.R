39572b3dc9e6cfb99eb5be6c33f65bb317a06149
amantaya <aantaya@email.arizona.edu>
2023-04-25 13:28:26 -0700

    remove unused "leaflet" package and add namespace to func

--- scripts/utilities/extract-image-metadata.R
+++ scripts/utilities/extract-image-metadata.R
@@ -1,12 +1,11 @@
 
 library(exifr)
 library(dplyr)
-library(leaflet)
 
 currentfolder<-getwd()
 
 files <- list.files(path=getwd(),full.names=T,pattern=c(".JPG|.jpg"),include.dirs = T,recursive=T)
-dat <- read_exif(files)
+dat <- exifr::read_exif(files)
 
 excelfilename<-paste(rev(strsplit(currentfolder,split="/")[[1]])[1],".csv",sep="")
 
