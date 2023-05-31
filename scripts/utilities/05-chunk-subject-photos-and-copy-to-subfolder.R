2d3323d36ebf3ab394f851c01e1b411eec83ec02
amantaya <aantaya@email.arizona.edu>
2023-05-31 10:01:28 -0700

    change setup to use `here::here()` instead of tilde expansion

--- scripts/utilities/05-chunk-subject-photos-and-copy-to-subfolder.R
+++ scripts/utilities/05-chunk-subject-photos-and-copy-to-subfolder.R
@@ -23,13 +23,13 @@
 rm(list = ls(all.names = TRUE))
 
 # set the working directory and environment variables
-source("~/grazing-interaction/environment.R")
+source(here::here("environment.R"))
 
 # load in the required packages
-source("~/grazing-interaction/packages.R")
+source(here::here("packages.R"))
 
 # load in the required functions
-source("~/grazing-interaction/functions.R")
+source(here::here("functions.R"))
 
 # Select Folders to Extract -----------------------------------------------
 
