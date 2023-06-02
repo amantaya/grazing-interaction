d0bcc65ac16104d10b040a9af522f2d30b618b82
amantaya <aantaya@email.arizona.edu>
2023-06-02 14:09:51 -0700

    lint 06-copy-chunk-to-blank-macro.R

--- scripts/utilities/06-copy-chunk-to-blank-macro.R
+++ scripts/utilities/06-copy-chunk-to-blank-macro.R
@@ -26,7 +26,7 @@ if (project_switch == "heber") {
 } else if (project_switch == "white mountains") {
   blankmacro <- "HorseImaging-2020-White-Mountains.xlsm"
 } else if (project_switch == "heber motion") {
-    blankmacro <- "HorseImaging-2021-Heber-Motion.xlsm"
+  blankmacro <- "HorseImaging-2021-Heber-Motion.xlsm"
 } else {
   warning("The project has not been selected or was entered incorrectly.")
 }
@@ -52,56 +52,87 @@ project_kanban <-
 
 # heading patterns to find
 
-copy_to_blank_macro_heading_regex <- "##\\sFolders\\sto\\sCopy\\sinto\\sBlank\\sMacro"
+copy_to_blank_macro_heading_regex <-
+  "##\\sFolders\\sto\\sCopy\\sinto\\sBlank\\sMacro"
 
-upload_to_box_scoring_heading_regex <- "##\\sFolders\\sto\\sUpload\\sto\\sBox\\sfor\\sSCORING"
+upload_to_box_scoring_heading_regex <-
+  "##\\sFolders\\sto\\sUpload\\sto\\sBox\\sfor\\sSCORING"
 
+# TODO rename `heber_project_kanban` to `project_kanban`
 # create and index of the two headings
 # we want what's in between the headings
-copy_to_blank_macro_heading_index <- stringr::str_which(heber_project_kanban, pattern = copy_to_blank_macro_heading_regex)
+copy_to_blank_macro_heading_index <-
+  stringr::str_which(
+    heber_project_kanban,
+    pattern = copy_to_blank_macro_heading_regex
+  )
 
-upload_to_box_scoring_heading_index <- stringr::str_which(heber_project_kanban, pattern = upload_to_box_scoring_heading_regex)
+upload_to_box_scoring_heading_index <-
+  stringr::str_which(
+    heber_project_kanban,
+    pattern = upload_to_box_scoring_heading_regex
+  )
 
 # add 1 because we don't want to include the first heading
 # subtract 1 because we don't want to include the last heading
 # subset the kanban board using these indexes
 # TODO should I consider not using the index?
-folders_to_chunk <- heber_project_kanban[(copy_to_blank_macro_heading_index + 2):(upload_to_box_scoring_heading_index - 2)]
+folders_to_chunk <-
+  heber_project_kanban[
+    (copy_to_blank_macro_heading_index + 2):
+    (upload_to_box_scoring_heading_index - 2)
+  ]
 
 folders_to_chunk_regex_pattern <-
-  "([[:upper:]][[:upper:]][[:upper:]]_\\d{8}_\\d{8}|[[:upper:]]\\d{2}_\\d{8}_\\d{8})"
+  "([[:upper:]][[:upper:]][[:upper:]]_\\d{8}_\\d{8}|[[:upper:]]\\d{2}_\\d{8}_\\d{8})" # nolint: line_length_limit
 
 folders_to_chunk_pattern_matches <-
   stringr::str_extract(folders_to_chunk,
                        pattern = folders_to_chunk_regex_pattern)
 
 # return only the pattern matches that were not NA
-folders_to_chunk <- folders_to_chunk_pattern_matches[is.na(folders_to_chunk_pattern_matches) == FALSE]
+folders_to_chunk <-
+  folders_to_chunk_pattern_matches[
+    is.na(folders_to_chunk_pattern_matches) == FALSE
+  ]
 
-# create a data frame with a "site" column that we can use to construct file paths
+# create a data frame with a "site" column
+# that we can use to construct file paths
 cameratraps_folders_to_chunk <-
   cameratraps_path_constructor(folders_to_chunk)
 
 # Copy Subjects to Blank Macro --------------------------------------
 
-for (i in 1:nrow(cameratraps_folders_to_chunk)) {
+for (i in seq_len(nrow(cameratraps_folders_to_chunk))) {
 
 # list all of the chunk subfolders in the collection
-subfolders_in_collection_folder_relative_path <- list.dirs(cameratraps_folders_to_chunk$full_path[i], full.names = FALSE)
+subfolders_in_collection_folder_relative_path <-
+  list.dirs(
+    cameratraps_folders_to_chunk$full_path[i],
+    full.names = FALSE
+  )
 
-subfolders_in_collection_folder_absolute_path <- list.dirs(cameratraps_folders_to_chunk$full_path[i])
+subfolders_in_collection_folder_absolute_path <-
+  list.dirs(
+    cameratraps_folders_to_chunk$full_path[i]
+  )
 
 # create a wildcard pattern that we can use to match just the "chunk" subfolders
 chunk_wildcard <- "chunk*"
 
-# use a global regular expression to match and return just the "chunk" subfolders
-chunk_subfolders_in_collection_folder <- grep(chunk_wildcard,
-                                              subfolders_in_collection_folder_relative_path,
-                                              value = TRUE)
+# use a global regular expression to match
+# and return just the "chunk" subfolders
+chunk_subfolders_in_collection_folder <-
+  grep(chunk_wildcard,
+    subfolders_in_collection_folder_relative_path,
+    value = TRUE
+  )
 
-chunk_subfolders_absolute_paths <- grep(chunk_wildcard,
-                                        subfolders_in_collection_folder_absolute_path,
-                                        value = TRUE)
+chunk_subfolders_absolute_paths <-
+  grep(chunk_wildcard,
+    subfolders_in_collection_folder_absolute_path,
+    value = TRUE
+  )
 
 # number of chunks in the collection folder
 # use this object to indexing
@@ -113,16 +144,21 @@ for (j in (1:n_chunks_in_collection_folder)) {
 
   chunk_name <- chunk_subfolders_in_collection_folder[j]
 
-  path_to_chunk_csv <- file.path(cameratraps_folders_to_chunk$full_path[i],
-                                 "metadata",
-                                 paste0(cameratraps_folders_to_chunk$collection_folder[i],
-                                        "_subjects_", chunk_name, ".csv"))
+  path_to_chunk_csv <-
+    file.path(
+      cameratraps_folders_to_chunk$full_path[i],
+      "metadata",
+      paste0(
+        cameratraps_folders_to_chunk$collection_folder[i],
+        "_subjects_", chunk_name, ".csv"
+      )
+    )
 
   chunk1_csv <- readr::read_csv(path_to_chunk_csv)
 
   # keep only the columns that we want
-  # "RecordNumber, ImageFilename, ImagePath, ImageRelative, ImageSize, ImageTime, ImageDate, ImageAlert"
-  chunk1_discard_columns <- chunk1_csv %>%
+  chunk1_discard_columns <-
+    chunk1_csv %>%
     dplyr::select(
       RecordNumber,
       ImageFilename,
@@ -134,7 +170,8 @@ for (j in (1:n_chunks_in_collection_folder)) {
       ImageAlert
     )
 
-  # copy the template excel macro from the git repo into the camera trap folder on the external hard drive
+  # copy the template excel macro from the git repo
+  # into the camera trap folder on the external hard drive
 
   from <-
     file.path(currentwd,
@@ -148,61 +185,85 @@ for (j in (1:n_chunks_in_collection_folder)) {
 
   # TODO Add Warning Message If File Is Not Copied
 
-  # trim the "/home/rstudio/cameratraps/wildcat/exclosure/WCX_05212019_07102019/101EK113" from the "ImageRelative" column
-  # we want to keep just the file name because the excel macro is going to default to relative image paths
-  # and altering the rel path to just the filename will tell the macro to look inside just the "chunk" folder
   chunk1_relpaths_split_into_list <-
-    stringr::str_split(chunk1_discard_columns$ImageRelative, "/")
+    stringr::str_split(
+      chunk1_discard_columns$ImageRelative,
+      "/"
+    )
 
-  keep.last.split.in.file.path <- function(character_vector, pattern) {
-    # create an empty object to hold the output of the function
-    keep_last_object <- NULL
+  # TODO extract to function and test
+  # is just the same thing as `basename()`?
+  keep.last.split.in.file.path <-
+    function(character_vector, pattern) {
+      # create an empty object to hold the output of the function
+      keep_last_object <- NULL
 
-    # string split the character vector on a character or pattern
-    all_subjects_string_split <-
-      stringr::str_split(character_vector, pattern)
+      # string split the character vector on a character or pattern
+      all_subjects_string_split <-
+        stringr::str_split(character_vector, pattern)
 
-    # lengths() detects the number of objects in each list
-    # create an index for the last object in a list using the lengths() func
-    num_data_objects <- lengths(all_subjects_string_split)
+      # lengths() detects the number of objects in each list
+      # create an index for the last object in a list using the lengths() func
+      num_data_objects <- lengths(all_subjects_string_split)
 
-    last_object <- num_data_objects
+      last_object <- num_data_objects
 
-    # index the list and keep only the last object
-    for (i in 1:length(all_subjects_string_split)) {
-      keep_last_object[i] <-
-        all_subjects_string_split[[i]][last_object[i]]
+      # index the list and keep only the last object
+      for (i in seq_along(all_subjects_string_split)) {
+        keep_last_object[i] <-
+          all_subjects_string_split[[i]][last_object[i]]
+      }
+      return(keep_last_object)
     }
-    return(keep_last_object)
-  }
 
   chunk1_discard_columns$ImageRelative <-
-    keep.last.split.in.file.path(chunk1_discard_columns$ImageRelative, "/")
+    keep.last.split.in.file.path(
+      chunk1_discard_columns$ImageRelative,
+      "/"
+    )
 
   # name the macro by the chunk number
   from <-
-    file.path(cameratraps_folders_to_chunk$full_path[i],
-              chunk_name,
-              blankmacro)
+    file.path(
+      cameratraps_folders_to_chunk$full_path[i],
+      chunk_name,
+      blankmacro
+    )
 
   xlsm_file_name <-
-    paste0(cameratraps_folders_to_chunk$collection_folder[i], "_subjects_", chunk_name, ".xlsm")
+    paste0(
+      cameratraps_folders_to_chunk$collection_folder[i],
+      "_subjects_",
+      chunk_name,
+      ".xlsm"
+    )
 
   to <-
-    file.path(cameratraps_folders_to_chunk$full_path[i], chunk_name, xlsm_file_name)
+    file.path(
+      cameratraps_folders_to_chunk$full_path[i],
+      chunk_name,
+      xlsm_file_name
+    )
 
   file.rename(from = from, to = to)
 
   # TODO Add Warning Message if File Is Not Renamed
 
   # copy and paste the data from the csv file into the macro
-  # the data from the csv file has been corrected and the unnecessary columns dropped
+  # the data from the csv file has been corrected
+  # and the unnecessary columns dropped
   xlsm_workbook <-
-    openxlsx::loadWorkbook(file.path(cameratraps_folders_to_chunk$full_path[i], chunk_name, xlsm_file_name))
+    openxlsx::loadWorkbook(
+      file.path(
+        cameratraps_folders_to_chunk$full_path[i],
+        chunk_name,
+        xlsm_file_name
+      )
+    )
 
   chunk1_discard_columns_df <- as.data.frame(chunk1_discard_columns)
 
-  # somewhat confusingly, "writeData" writes data to workbook object bound to R
+  # "writeData" writes data to workbook object bound to R
   # but those changes are not yet saved to the file
   openxlsx::writeData(
     xlsm_workbook,
@@ -216,17 +277,23 @@ for (j in (1:n_chunks_in_collection_folder)) {
   # write out a filled macro
   openxlsx::saveWorkbook(
     xlsm_workbook,
-    file.path(cameratraps_folders_to_chunk$full_path[i], chunk_name, xlsm_file_name),
+    file.path(
+      cameratraps_folders_to_chunk$full_path[i],
+      chunk_name,
+      xlsm_file_name
+    ),
     overwrite = TRUE
   )
 }
 
 # create a new folder to use as a temporary folder for uploading to the cloud
-temp_folder_for_uploading_chunk <- file.path(currentwd,
-                                             "data",
-                                             "temp",
-                                             "chunks",
-                                             cameratraps_folders_to_chunk$collection_folder[i])
+temp_folder_for_uploading_chunk <- file.path(
+  currentwd,
+  "data",
+  "temp",
+  "chunks",
+  cameratraps_folders_to_chunk$collection_folder[i]
+)
 
 dir.create(temp_folder_for_uploading_chunk)
 
@@ -239,7 +306,7 @@ to <- temp_folder_for_uploading_chunk
 # copy the chunks onto my local drive for upload
 file.copy(from = from, to = to, recursive = TRUE)
 
-# toc()
+# TODO add code sections
 
 system_time <- Sys.time()
 
